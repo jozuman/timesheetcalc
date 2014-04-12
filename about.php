37d153f3048ee6bffbacb13cbd25451026bbd01c
jozuman <tim.grimes.3@gmail.com>
2014-04-11 21:02:42 -0500

    added about page and setup debugging option instead of just var_dumping everywhere

---
+++ about.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="jozuman">
+    <title>Timesheet Calculator</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Custom styles for this template -->
+    <link href="css/template.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Timesheet Calc</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li class="active"><a href="about.php">About</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <p>this is a simple time summing webapp I made in a few hours to easily calculate how much time I spend on stuff.</p>
+        <p>the idea came from: <a href="http://timesheetcalc.com/">http://timesheetcalc.com/</a></p>
+        <p>this source code can be found at: <a href="https://github.com/jozuman/timesheetcalc/">https://github.com/jozuman/timesheetcalc/</a></p>
+        <p>Also! AM and PM are not required if the time is during business hours (6AM - 5:59PM). It will assume the correct time.</p>
+      </di>
+
+    </div><!-- /.container -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
+
--- index.php
+++ index.php
@@ -31,13 +31,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Timesheet Calc</a>
+          <a class="navbar-brand" href="index.php">Timesheet Calc</a>
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li class="active"><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
@@ -47,11 +46,15 @@
 
       <div class="starter-template">
         <h1>Times</h1>
-        <p class="lead">Paste.<br> Go.</p>
+        <p class="lead">Paste the time spent on something<br>Go. will sum it.</p>
         <form method="post" action="index.php" class="form">
             <input type="text" class="form-control" placeholder="hh:mm - hhPM, hh:mm - hh:mm" name="data">
             <br>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <button type="submit" class="btn btn-default">Go.</button>
+            <br>
+            <div class="pull-right text-muted">
+                <input type="checkbox" name="debug" value="true"> <small>debugging</small></input>
+            </div>
         </form>
       </div>
       
@@ -105,8 +108,10 @@
 
                 $endDateTime = DateTime::createFromFormat(calculateTimeFormat($end), $end);
 
-                var_dump($startDateTime);
-                var_dump($endDateTime);
+                if($_POST["debug"]){
+                    var_dump($startDateTime);
+                    var_dump($endDateTime);
+                }
                 $interval = date_diff($startDateTime, $endDateTime);
                 $actualTime = $interval->h + $interval->i/60;
                 $actualTimes[] = $actualTime;
